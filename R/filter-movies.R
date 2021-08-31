99b57dcf4898d17543fec45b42653ddb0a1d8a4d
Ron Au <5785323+ronvoluted@users.noreply.github.com>
2021-09-01 00:27:54 +1000

    feat: Simplify initialisation of filtered movies

--- R/filter-movies.R
+++ R/filter-movies.R
@@ -7,9 +7,7 @@ library(lubridate)
 
 # Filter saved movies ----
 
-movies <- read_tsv("data/all_top_rated_movies_2021-08-28.tsv")
-
-filtered_movies <- movies %>%
+filtered_movies <- read_tsv("data/all_top_rated_movies_2021-08-28.tsv") %>%
   filter(
     year(.data[["release_date"]]) == 2019,
     vote_count > 500,
